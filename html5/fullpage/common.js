cf267ac30e452999eb8d877097615c1ca5d8b26d
zll18314831310 <18314831310@163.com>
2018-06-13 23:24:27 +0800

    全屏滚动插件原生js造轮子

--- html5/fullpage/common.js
+++ html5/fullpage/common.js
@@ -1,8 +1,20 @@
 class PureFullPage {
+    // options 是插件的配置
     constructor(options) {
+        const defaultOptions = {
+            isShowNav: true,
+            delay: 1000,
+            // 每次turnpage callback
+            definePages: ()=> {}
+        }
+        this.currentPage = 0;
+        this.currentPosition = 0;
+        this.options = Object.assign(defaultOptions,options);
+        console.log(this.options)
         this.viewHeight = window.innerHeight;
         this.pureFullPage = document.querySelector('#pureFullPage');
         this.page = document.querySelectorAll('.page');
+        this.DELAY = this.options.delay;
         this.init();
     }
     init() {
@@ -13,26 +25,49 @@ class PureFullPage {
         // 重新返回一个函数，handleMouseWheel，闭包，将this.scrollMouse 封到内部 
         // this,函数执行的context
         // 1000 delay 推迟执行的时间
-        const handleMouseWheel = utils.throttle(this.scrollMouse,this,1500);
+        const handleMouseWheel = utils.throttle(this.scrollMouse,this,this.DELAY);
         if(navigator.userAgent.toLowerCase().indexOf('firefox')===-1) {
             document.addEventListener('mousewheel',handleMouseWheel,false);
         }else {
             document.addEventListener('DOMMouseScroll',handleMouseWheel,false);
         }
+
+        // 事件处理函数交给对象的方法来执行，
+        window.addEventListener('resize',this.handleWindowResize.bind(this),false);
+    }
+    handleWindowResize(e) {
+        
+        // 防抖， 
+        utils.debounce(this.getNewPosition,this,event,this.DELAY);
+    }
+    getNewPosition() {
+        this.viewHeight = window.innerHeight;
+        this.pureFullPage.style.height = `${this.viewHeight}px`;
+        this.pureFullPage.style.top = `-${this.currentPage*this.viewHeight}px`;
     }
     // 滚轮事件处理函数
     scrollMouse(e) {
-        console.log(this.pureFullPage.offsetTop)
-        let viewHeight = this.viewHeight;
-        let top = parseInt(getComputedStyle(this.pureFullPage,null).top);
+        // this.handleWindowResize();
+        const delta = utils.getWheelDelta(event);
+        if(delta>0) {
+            this.goUp();
+        }else {
+            this.goDown();
+        }
+    }
+    goUp() {
+        if(this.currentPage==0) {
+            return
+        }
+        this.currentPage--;
+        this.pureFullPage.style.top = `-${this.currentPage*this.viewHeight}px`;
         
-        // for(let i=1;i<this.page.length;i++) {
-            if(e[0].deltaY>0) {
-                this.pureFullPage.style.top = (top-viewHeight)+'px'; 
-            }else {
-                this.pureFullPage.style.top = (top+viewHeight)+'px'; 
-            }
-
-
+    }
+    goDown() {
+        if(this.currentPage==3) {
+            return
+        }
+        this.currentPage++;
+        this.pureFullPage.style.top = `-${this.currentPage*this.viewHeight}px`;
     }
 }
--- html5/fullpage/utils.js
+++ html5/fullpage/utils.js
@@ -1,3 +1,8 @@
+// 截流 throttle 
+// 防抖 debounce 在规定时间执行一次，后执行
+// 都为解决高频事件而制定，scroll mousewheel mousemover 
+// mousemove touchmove onresize
+
 const utils = {
     // 帮忙method执行的次数在规定时间内还有一次
     // method执行时，函数内的this 一定要指向我们的PureFullPage 类
@@ -10,14 +15,28 @@ const utils = {
         return function(...args) {
             // console.log(args)
             if(!wait) {
-                method.call(context,args)
+                method.apply(context,args)
                 wait = true;
-                clearTimeout(timer)
-                var timer=setTimeout(()=> {
+                setTimeout(()=> {
                     wait = false;
                 },delay)
             }
             
         }
+    },
+    debounce(method,context,event,delay) {
+        // window.resize 不要急，等一下再执行
+        clearTimeout(context.tId);
+        context.tId = setTimeout(()=>{
+            method.call(context,event)
+        },delay)
+    },
+    getWheelDelta(event) {
+        // console.log(event)
+        if(event.wheelDelta) {
+            return event.wheelDelta;
+        }
+        // chrome
+     
     }
 }
