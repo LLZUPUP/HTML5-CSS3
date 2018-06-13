cfa5d2890a9add71bc7a477938a5f711d90463d6
zll18314831310 <18314831310@163.com>
2018-06-13 14:11:46 +0800

    talk_car js

--- html5/talk_car/car.js
+++ html5/talk_car/car.js
@@ -4,8 +4,8 @@ window.onload = function() {
     let data = container.querySelectorAll(".data");
     let play1 = btn[0].querySelector("i");
     let play2 = btn[1].querySelector("i");
-    console.log(play1)
-    console.log(play2)
+    let time = document.querySelectorAll(".current-time");
+    let duration = document.querySelectorAll(".duration");
     let audio_1 = document.querySelector("#play_0");
     let audio_2 = document.querySelector("#play_1");
     // let player_1_url = audio_1.dataset.urls;
@@ -22,21 +22,36 @@ window.onload = function() {
             play1.className="play";
             audio_1.pause();
             pause = true;
+            time[0].style.display= "none";
+            duration[0].style.display = "inline";
             return
         }else {
             play1.className="pause";
             audio_1.play();
             pause = false;
-        console.log(currentTime)
-
+            time[0].style.display= "inline";
+            duration[0].style.display = "none";
+            var ii = setInterval(function(){
+                cur = audio_1.currentTime;
+                data[0].style.width=""+(cur/39)*100+"%";   
+            },1000) 
+            console.log(cur)
+            var oo = setInterval(function(){
+                rrr = parseInt(audio_1.currentTime);
+                var temp = rrr;
+                minute = parseInt(temp/60); 
+                if(rrr%60<10){
+                    time[0].innerHTML="0"+minute+":0"+rrr%60+""
+                }else{
+                    time[0].innerHTML="0"+minute+":"+rrr%60+""
+                }
+            },100)
+            
         }
-        
-        data[0].style.width=`${currentTime}%`;
-       
-        console.log(parseInt(audio_1.currentTime))
-        console.log(parseInt(audio_1.duration))
-        
     },false)
+
+
+
     let pause2 = true;
     btn[1].addEventListener("click",function() {
         if(pause) {
